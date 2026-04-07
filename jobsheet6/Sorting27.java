2f2d54138c31c100cd56086259c47fa2ffc92542
tezabae <tesalonikabenila@gmail.com>
2026-04-07 19:56:42 +0700

    Sorting

--- jobsheet6/Sorting27.java
+++ jobsheet6/Sorting27.java
@@ -3,12 +3,15 @@ package jobsheet6;
 public class Sorting27 {
     int[] data;
     int size;
-
-    public Sorting27(int[] data) {
-        this.data = data;
-        this.size = data.length;
+    
+    public Sorting27(int[] data, int size) {
+        this.size = size;
+        this.data = new int[size];
+        for (int i = 0; i < size; i++) {
+            this.data[i] = data[i];
+        }
     }
-
+    
     public void bubbleSort() {
         for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size - i - 1; j++) {
@@ -20,10 +23,36 @@ public class Sorting27 {
             }
         }
     }
-
+    
+    public void selectionSort() {
+        for (int i = 0; i < size - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < size; j++) {
+                if (data[j] < data[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = data[minIdx];
+            data[minIdx] = data[i];
+            data[i] = temp;
+        }
+    }
+    
+    public void insertionSort() {
+        for (int i = 1; i < size; i++) {
+            int temp = data[i];
+            int j = i - 1;
+            while (j >= 0 && data[j] > temp) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = temp;
+        }
+    }
+    
     public void print() {
-        for (int num : data) {
-            System.out.print(num + " ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(data[i] + " ");
         }
         System.out.println();
     }
--- jobsheet6/SortingMain27.java
+++ jobsheet6/SortingMain27.java
@@ -2,19 +2,31 @@ package jobsheet6;
 
 public class SortingMain27 {
 
-public class Main {
+public class SortingMain11 {
     public static void main(String[] args) {
-        int[] arr = {34, 7, 23, 32, 5, 62};
-        Sorting27 sorter = new Sorting27(arr);
-
-        System.out.println("Original aray:");
-        sorter.print();
-
-        sorter.bubbleSort();
-
-        System.out.println("Sorted array(bubble sort):");
-        sorter.print();
+        int[] a = {34, 7, 23, 32, 5, 62};
+        Sorting27 sorting1 = new Sorting27(a, a.length);
+        System.out.println("Original array:");
+        sorting1.print();
+        sorting1.bubbleSort();
+        System.out.println("Sorted array (Bubble Sort):");
+        sorting1.print();
+        
+        int[] b = {30, 20, 2, 8, 14};
+        Sorting27 sorting2 = new Sorting27(b, b.length);
+        System.out.println("\nOriginal array:");
+        sorting2.print();
+        sorting2.selectionSort();
+        System.out.println("Sorted array (Selection Sort):");
+        sorting2.print();
+        
+        int[] c = {40, 10, 4, 9, 3};
+        Sorting27 sorting3 = new Sorting27(c, c.length);
+        System.out.println("\nOriginal array:");
+        sorting3.print();
+        sorting3.insertionSort();
+        System.out.println("Sorted array (Insertion Sort):");
+        sorting3.print();
     }
 }
-
 }
