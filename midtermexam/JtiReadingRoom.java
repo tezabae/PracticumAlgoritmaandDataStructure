e8a2b4654ffabf0bc41059a3847a97b4da517ef5
tezabae <tesalonikabenila@gmail.com>
2026-04-14 12:15:03 +0700

    midtermexam

--- midtermexam/JtiReadingRoom.java
+++ midtermexam/JtiReadingRoom.java
@@ -1,60 +1,50 @@
 package midtermexam;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class JtiReadingRoom {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input jumlah data
+        // Input Students
         System.out.print("Enter number of students: ");
-        int nStudents = sc.nextInt();
-        sc.nextLine();
+        int nStudents = sc.nextInt(); sc.nextLine();
         Student[] students = new Student[nStudents];
         for (int i = 0; i < nStudents; i++) {
             System.out.println("Student " + (i+1) + ":");
-            System.out.print("ID: ");
-            String id = sc.nextLine();
-            System.out.print("Name: ");
-            String name = sc.nextLine();
-            System.out.print("Study Program: ");
-            String prog = sc.nextLine();
+            System.out.print("ID: "); String id = sc.nextLine();
+            System.out.print("Name: "); String name = sc.nextLine();
+            System.out.print("Study Program: "); String prog = sc.nextLine();
             students[i] = new Student(id, name, prog);
         }
 
+        // Input Books
         System.out.print("\nEnter number of books: ");
-        int nBooks = sc.nextInt();
-        sc.nextLine();
+        int nBooks = sc.nextInt(); sc.nextLine();
         Book[] books = new Book[nBooks];
         for (int i = 0; i < nBooks; i++) {
             System.out.println("Book " + (i+1) + ":");
-            System.out.print("Code: ");
-            String code = sc.nextLine();
-            System.out.print("Title: ");
-            String title = sc.nextLine();
-            System.out.print("Year: ");
-            int year = sc.nextInt(); sc.nextLine();
+            System.out.print("Code: "); String code = sc.nextLine();
+            System.out.print("Title: "); String title = sc.nextLine();
+            System.out.print("Year: "); int year = sc.nextInt(); sc.nextLine();
             books[i] = new Book(code, title, year);
         }
 
+        // Input Loans
         System.out.print("\nEnter number of loans: ");
-        int nLoans = sc.nextInt();
-        sc.nextLine();
+        int nLoans = sc.nextInt(); sc.nextLine();
         Loan[] loans = new Loan[nLoans];
         for (int i = 0; i < nLoans; i++) {
             System.out.println("Loan " + (i+1) + ":");
-            System.out.print("Student ID: ");
-            String id = sc.nextLine();
+            System.out.print("Student ID: "); String id = sc.nextLine();
             Student std = findStudent(students, id);
-            System.out.print("Book Code: ");
-            String code = sc.nextLine();
+            System.out.print("Book Code: "); String code = sc.nextLine();
             Book bk = findBook(books, code);
-            System.out.print("Loan Duration (days): ");
-            int dur = sc.nextInt(); sc.nextLine();
+            System.out.print("Loan Duration (days): "); int dur = sc.nextInt(); sc.nextLine();
             loans[i] = new Loan(std, bk, dur);
         }
 
+        // Menu
         int choice;
         do {
             System.out.println("\n=== Loan Management System in JTI Reading Room ===");
@@ -70,15 +60,11 @@ public class JtiReadingRoom {
             switch (choice) {
                 case 1:
                     System.out.println("\nStudent List:");
-                    for (Student s : students)
-                        System.out.println("Id: " + s.id + " | Name: " + s.name +
-                                " | Study Program: " + s.studyProgram);
+                    for (Student s : students) s.showStudent();
                     break;
                 case 2:
                     System.out.println("\nBook List:");
-                    for (Book b : books)
-                        System.out.println("Code: " + b.code + " | Title: " + b.title +
-                                " | Year: " + b.year);
+                    for (Book b : books) b.showBook();
                     break;
                 case 3:
                     System.out.println("\nLoan List:");
@@ -136,8 +122,14 @@ public class JtiReadingRoom {
             int mid = (left + right) / 2;
             if (loans[mid].std.id.equals(id)) {
                 System.out.println("Loan(s) for Student ID " + id + ":");
-                for (Loan l : loans) if (l.std.id.equals(id)) l.showLoan();
-                found = true; break;
+                for (int i = 0; i < loans.length; i++) {
+                    if (loans[i].std.id.equals(id)) {
+                        System.out.println("Index: " + i);
+                        loans[i].showLoan();
+                    }
+                }
+                found = true;
+                break;
             } else if (loans[mid].std.id.compareTo(id) < 0) left = mid + 1;
             else right = mid - 1;
         }
