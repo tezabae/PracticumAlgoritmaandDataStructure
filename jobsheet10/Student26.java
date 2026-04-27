e3298cead4411d20ff2687cf53b81a13f0f56b56
tezabae <tesalonikabenila@gmail.com>
2026-04-27 22:10:29 +0700

    2.1.1

--- jobsheet10/Student26.java
+++ jobsheet10/Student26.java
@@ -6,20 +6,20 @@ public class Student26 {
     String className;
     int grade;
 
-    // Constructor tanpa parameter
+    // Constructor without parameters
     public Student26() {
         this.grade = -1;
     }
 
-    // Constructor dengan parameter
+    // Constructor with parameter
     public Student26(String nim, String name, String className) {
         this.nim = nim;
         this.name = name;
         this.className = className;
-        this.grade = -1; // default belum dinilai
+        this.grade = -1; // default have not been graded yet
     }
 
-    // Method untuk memberi nilai
+    // Method for grading
     void grading(int grade) {
         this.grade = grade;
     } 
